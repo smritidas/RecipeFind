23e35914340bb8d8b203152531e761122bf33a0f
Smriti Das <smritidas@users.noreply.github.com>
2021-09-02 16:08:39 -0400

    Refactor model

--- app/src/main/java/com/example/android/recipefind/RecipeApiService.kt
+++ app/src/main/java/com/example/android/recipefind/RecipeApiService.kt
@@ -1,10 +1,8 @@
 package com.example.android.recipefind
 
-import com.example.android.recipefind.BuildConfig.APP_ID
-import com.example.android.recipefind.models.Recipe
+import com.example.android.recipefind.data.model.Recipe
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 
 interface RecipeApiService {
