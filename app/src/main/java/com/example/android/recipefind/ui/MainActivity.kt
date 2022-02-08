21ac11ea69b6e4dd802aaf012baabf8b7ddbf058
Smriti Das <dassmrit@gmail.com>
2022-02-07 19:21:17 -0500

    Fix errors

--- app/src/main/java/com/example/android/recipefind/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/recipefind/ui/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navView: BottomNavigationView? = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
@@ -25,6 +25,6 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(setOf(
                 R.id.navigation_home, R.id.navigation_browse, R.id.navigation_search, R.id.navigation_bookmarks))
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView?.setupWithNavController(navController)
     }
 }
