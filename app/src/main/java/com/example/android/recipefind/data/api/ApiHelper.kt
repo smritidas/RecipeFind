c8af308f1117bf2be6d25c2bba659c89dd244cfd
smritidas <dassmrit@gmail.com>
2022-02-28 18:15:21 -0500

    Fixed fragment for XML

--- app/src/main/java/com/example/android/recipefind/data/api/ApiHelper.kt
+++ app/src/main/java/com/example/android/recipefind/data/api/ApiHelper.kt
@@ -1,5 +1,7 @@
 package com.example.android.recipefind.data.api
 
+
+// TODO rename this
 class ApiHelper(private val apiService: RecipeApiService) {
 
    suspend fun getRecipes() = apiService.getRecipe()
--- app/src/main/java/com/example/android/recipefind/data/repository/MainRepository.kt
+++ app/src/main/java/com/example/android/recipefind/data/repository/MainRepository.kt
@@ -4,6 +4,8 @@ import com.example.android.recipefind.data.api.ApiHelper
 import com.example.android.recipefind.data.model.Recipe
 import retrofit2.Response
 
+//TODO update for recipe api service to replace api helper
+
 class MainRepository(private val apiHelper: ApiHelper) {
 
     suspend fun getRecipes(): Response<List<Recipe>> {
