da5a79abec5e75061bdcb047391fd6c986af6465
smritidas <dassmrit@gmail.com>
2022-03-18 18:36:53 -0400

    Fixed HomeFragment

--- app/src/main/java/com/example/android/recipefind/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/recipefind/ui/MainActivity.kt
@@ -17,14 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView? = findViewById(R.id.nav_view)
+        //val navView: BottomNavigationView? = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_browse, R.id.navigation_search, R.id.navigation_bookmarks))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView?.setupWithNavController(navController)
+        
     }
 }
--- app/src/main/java/com/example/android/recipefind/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/recipefind/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.android.recipefind.R
 
 //TODO code up the buttons to point to other fragments
@@ -24,7 +25,7 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_browse, container, false)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
 
 
         val textView: TextView? = root.findViewById(R.id.text_home)
@@ -32,6 +33,11 @@ class HomeFragment : Fragment() {
             textView?.text = it
         })
         return root
+    }
+
+    private fun navigateToBrowse(){
+        findNavController().navigate(R.id.action_homeFragment_to_navigation_browse)
+            //wire up on click
     }
 
     //button to navigate to ideas ie browse fragment
