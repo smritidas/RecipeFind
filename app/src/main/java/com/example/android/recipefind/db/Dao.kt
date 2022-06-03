5a0bc86af2dcaf84d531f9ad4bf3438f744343d0
smritidas <dassmrit@gmail.com>
2022-06-03 14:44:17 -0400

    Add DAO

--- app/src/main/java/com/example/android/recipefind/db/Dao.kt
+++ app/src/main/java/com/example/android/recipefind/db/Dao.kt
@@ -1,9 +1,27 @@
 package com.example.android.recipefind.db
 
-class Dao {
+import androidx.room.*
+import androidx.room.Dao
+import com.example.android.recipefind.data.model.Recipe
 
-    //Data access object - maps SQL functions to queries
+//Data access object - maps SQL functions to queries
+//ASK should this use co-routines?
+@Dao
+interface Dao {
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(recipe: RecipeSaved)
 
+    @Update
+    suspend fun update(recipe: RecipeSaved)
+
+    @Delete
+    suspend fun delete(recipe: RecipeSaved)
+
+    @Query("DELETE FROM saved_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * from saved_table ORDER BY name ASC")
+    fun getAlphabetizedRecipes(): List<Recipe>
 
 }
